3c690521efdde318bb7151417bab7255ec97aa2d
MariaPGomezA <mariagomar@hotmail.com>
2021-07-18 17:09:56 -0500

    Estructura carrito

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carrito from './components/Carrito';
+
+function App() {
+  return (
+    <div>
+      <Carrito></Carrito>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/programacion.js
@@ -0,0 +1,4 @@
+// Carrito:
+// Verificar productos y cantidades. Opción de modificar
+// Cálculo de la cuenta: Subtotales y total
+// Botón: Caja registradora (siguiente paso)
