5cb6e907c7e1eaca28cc9c8bb8ab945715f11dc4
Juan Diego <jdquinterocg@gmail.com>
2021-07-18 22:59:02 -0500

    Importación de las páginas en el App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+
+import Header from './components/Header';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Carrito from './pages/Carrito';
+import User from './pages/User';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Login /> */}
-      {/* <Register /> */}
+      <Header></Header>
       <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
