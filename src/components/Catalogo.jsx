9493c8e694dce8b54b9e75f051e208e2d70737ed
claramarmfs <claramarmfs@gmail.com>
2021-07-21 16:56:05 -0500

    Catalogo

--- src/components/Catalogo.jsx
+++ src/components/Catalogo.jsx
@@ -4,6 +4,137 @@ export default function Catalogo() {
     return (
         <div>
             
+            { productocategoria: "varios" , 
+            productodescripcion2: "Arroz Diana 500g" , 
+            productoprecio: 1900 , 
+            productoimagen: "../imagenes/catalogo/101.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Lentejas Frescampo 500g" , 
+            productoprecio: 2710 , 
+            productoimagen: "../imagenes/catalogo/111.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Frijoles Frescampo 500g" , 
+            productoprecio: 3890 , 
+            productoimagen: "../imagenes/catalogo/113.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Maiz Pira Frescampo 500g" , 
+            productoprecio: 2800 , 
+            productoimagen: "../imagenes/catalogo/115.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Blanquillos Frescampo 500g" , 
+            productoprecio: 3410 , 
+            productoimagen: "../imagenes/catalogo/117.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Salero Sal Refrisal 130g" , 
+            productoprecio: 920 , 
+            productoimagen: "../imagenes/catalogo/121.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Aceite Frescampo 900cc" , 
+            productoprecio: 5880 , 
+            productoimagen: "../imagenes/catalogo/124.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Huevos 30unds" , 
+            productoprecio: 14000 , 
+            productoimagen: "../imagenes/catalogo/128.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Huevo 1und" , 
+            productoprecio: 600 , 
+            productoimagen: "../imagenes/catalogo/129.jpg" },
+
+            { productocategoria: "lacteos" , 
+            productodescripcion2: "Leche Alqueria Deslactosada 1100cc" , 
+            productoprecio: 3800 , 
+            productoimagen: "../imagenes/catalogo/220.jpg" },
+
+            { productocategoria: "lacteos" , 
+            productodescripcion2: "Queso Cuajada 500g" , 
+            productoprecio: 4500 , 
+            productoimagen: "../imagenes/catalogo/224.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Papas Margarita Natural 120g" , 
+            productoprecio: 4500 , 
+            productoimagen: "../imagenes/catalogo/302.jpg" },
+
+            { productocategoria: "frutasyverduras" , 
+            productodescripcion2: "Papa 2000g" , 
+            productoprecio: 9000 , 
+            productoimagen: "../imagenes/catalogo/401.jpg" },
+
+            { productocategoria: "frutasyverduras" , 
+            productodescripcion2: "Tomate 1000g" , 
+            productoprecio: 3500 , 
+            productoimagen: "../imagenes/catalogo/402.jpg" },
+
+            { productocategoria: "frutasyverduras" , 
+            productodescripcion2: "Cebolla 500g" , 
+            productoprecio: 3000 , 
+            productoimagen: "../imagenes/catalogo/403.jpg" },
+
+            { productocategoria: "frutasyverduras" , 
+            productodescripcion2: "Limon 1000g" , 
+            productoprecio: 3900 , 
+            productoimagen: "../imagenes/catalogo/404.jpg" },
+
+            { productocategoria: "frutasyverduras" , 
+            productodescripcion2: "Platano Maduro 1000g" , 
+            productoprecio: 2500 , 
+            productoimagen: "../imagenes/catalogo/408.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Botella Agua Brisa 600cc" , 
+            productoprecio: 1500 , 
+            productoimagen: "../imagenes/catalogo/502.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Gaseosa Postobon Manzana 1500cc" , 
+            productoprecio: 3000 , 
+            productoimagen: "../imagenes/catalogo/508.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Salchicha Rica 500g" , 
+            productoprecio: 5500 , 
+            productoimagen: "../imagenes/catalogo/601.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Paleta Uva" , 
+            productoprecio: 1500 , 
+            productoimagen: "../imagenes/catalogo/701.jpg" },
+
+            { productocategoria: "varios" , 
+            productodescripcion2: "Helado Lulo" , 
+            productoprecio: 3000 , 
+            productoimagen: "../imagenes/catalogo/713.jpg" },
+
+            { productocategoria: "aseo" , 
+            productodescripcion2: "Cepillo Dental Colgate" , 
+            productoprecio: 14000 , 
+            productoimagen: "../imagenes/catalogo/801.jpg" },
+
+            { productocategoria: "aseo" , 
+            productodescripcion2: "Escoba" , 
+            productoprecio: 10000 , 
+            productoimagen: "../imagenes/catalogo/809.jpg" },
+
+            { productocategoria: "aseo" , 
+            productodescripcion2: "Trapeador 250g" , 
+            productoprecio: 9500 , 
+            productoimagen: "../imagenes/catalogo/810.jpg" },
+
+            { productocategoria: "aseo" , 
+            productodescripcion2: "Jabon Johnson Avena 110g" , 
+            productoprecio: 1500 , 
+            productoimagen: "../imagenes/catalogo/817.jpg" }
+
+
         </div>
     )
 }
