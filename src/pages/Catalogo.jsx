1c1073665ce0a17cdc3e38628fcf7de024ba2ca5
Juan Diego <jdquinterocg@gmail.com>
2021-07-20 20:43:49 -0500

    Lista de productos

---
+++ src/pages/Catalogo.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Catalogo extends Component {
+
+    state = {
+        data: [
+            { categoria: "varios", descripcion: "Arroz Diana 500g", precio: 1900, imagen: "../imagenes/101.jpg"},
+            { categoria: "varios", descripcion: "Lentejas Frescampo 500g", precio: 2710, imagen: "../imagenes/111.jpg"},
+            { categoria: "varios", descripcion: "Frijoles Frescampo 500g", precio: 3890, imagen: "../imagenes/113.jpg"},
+            { categoria: "varios", descripcion: "Maiz Pira Frescampo 500g", precio: 2800, imagen: "../imagenes/115.jpg"},
+            { categoria: "varios", descripcion: "Blanquillos Frescampo 500g", precio: 3410, imagen: "../imagenes/117.jpg"},
+            { categoria: "varios", descripcion: "Salero Sal Refrisal 130g", precio: 920,  imagen: "../imagenes/121.jpg"},
+            { categoria: "varios", descripcion: "Aceite Frescampo 900cc", precio: 5880, imagen: "../imagenes/124.jpg"},
+            { categoria: "varios", descripcion: "Huevos 30unds", precio: 14000, imagen: "../imagenes/128.jpg"},
+            { categoria: "varios", descripcion: "Huevo 1und", precio: 600,  imagen: "../imagenes/129.jpg"},
+            { categoria: "lacteos" , descripcion: "Leche Alqueria Deslactosada 1100cc", precio: 3800, imagen: "../imagenes/220.jpg"},
+            { categoria: "lacteos" , descripcion: "Queso Cuajada 500g", precio: 4500, imagen: "../imagenes/224.jpg"},
+            { categoria: "varios", descripcion: "Papas Margarita Natural 120g", precio: 4500, imagen: "../imagenes/302.jpg"},
+            { categoria: "frutasyverduras", descripcion: "Papa 2000g", precio: 9000, imagen: "../imagenes/401.jpg"},
+            { categoria: "frutasyverduras", descripcion: "Tomate 1000g", precio: 3500, imagen: "../imagenes/402.jpg"},
+            { categoria: "frutasyverduras", descripcion: "Cebolla 500g", precio: 3000, imagen: "../imagenes/403.jpg"},
+            { categoria: "frutasyverduras", descripcion: "Limon 1000g", precio: 3900, imagen: "../imagenes/404.jpg"},
+            { categoria: "frutasyverduras", descripcion: "Platano Maduro 1000g", precio: 2500, imagen: "../imagenes/408.jpg"},
+            { categoria: "varios", descripcion: "Botella Agua Brisa 600cc", precio: 1500, imagen: "../imagenes/502.jpg"},
+            { categoria: "varios", descripcion: "Gaseosa Postobon Manzana 1500cc", precio: 3000, imagen: "../imagenes/508.jpg"},
+            { categoria: "varios", descripcion: "Salchicha Rica 500g", precio: 5500, imagen: "../imagenes/601.jpg"},
+            { categoria: "varios", descripcion: "Paleta Uva", precio: 1500, imagen: "../imagenes/701.jpg"},
+            { categoria: "varios", descripcion: "Helado Lulo", precio: 3000, imagen: "../imagenes/713.jpg"},
+            { categoria: "aseo", descripcion: "Cepillo Dental Colgate", precio: 4000, imagen: "../imagenes/801.jpg"},
+            { categoria: "aseo", descripcion: "Escoba", precio: 10000, imagen: "../imagenes/809.jpg"},
+            { categoria: "aseo", descripcion: "Trapeador 250g", precio: 9500, imagen: "../imagenes/810.jpg"},
+            { categoria: "aseo", descripcion: "Jabon Johnson Avena 110g", precio: 1500, imagen: "../imagenes/817.jpg"}
+            ]
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Catalogo;
