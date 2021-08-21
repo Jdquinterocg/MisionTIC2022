5ceec42da2750837e3c737c71cca658875b0da98
Juan Diego <jdquinterocg@gmail.com>
2021-08-21 07:38:40 -0500

    Private route user

--- src/App.js
+++ src/App.js
@@ -51,7 +51,6 @@ function App() {
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
-            <Route exact path="/domicilios" component={User} />
             <Route exact path="/domi" component={CajaDomi} />
             <Route exact path="/recoge" component={CajaRecoge} />
             <Route exact path="/confirmaRecoge" component={ConfirmaRecogida} />
@@ -62,6 +61,7 @@ function App() {
             <Route exact path="/varios" component={Varios} />
 
             <PrivateRoute exact path="/compras" component={Carrito} />
+            <PrivateRoute exact path="/domicilios" component={User} />
           </Switch>
         </Layout>
       </BrowserRouter>
